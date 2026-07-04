79171302f6ee030e22fcbcc21e0e4e2c52dae1a1
Kassem <kassem.ak@gmail.com>
2026-07-04 21:27:23 +0200

    Revert "first commit"
    This reverts commit e1d69cf5f54546a314a292cf14015090bcef5636.

--- app/src/main/java/com/spotter/app/ui/screens/ChatScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.spotter.app.ui.screens
-
-import androidx.compose.runtime.Composable
-import com.spotter.app.state.SpotterViewModel
-
-@Composable
-fun ChatScreen(vm: SpotterViewModel) {
-    ScreenPlaceholder("Chat")
-}
--- app/src/main/java/com/spotter/app/ui/screens/CommunityScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.spotter.app.ui.screens
-
-import androidx.compose.runtime.Composable
-import com.spotter.app.state.SpotterViewModel
-
-@Composable
-fun CommunityScreen(vm: SpotterViewModel) {
-    ScreenPlaceholder("Community")
-}
